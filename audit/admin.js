1684a3d6b320611ba18d807c6f4656b80917b3a9
Adrian Green <adriangreen@Adrians-MacBook-Air.local>
2014-07-01 17:59:31 -0400

    counter issues

--- audit/admin.js
+++ audit/admin.js
@@ -144,7 +144,7 @@ app.post('/details', function(req,res){
                                 if(err){console.log(err)}
                                 
                                 //res.send(row_data);
-                                console.log(row_data._xml);
+                                console.log(row_data);
                                 var parser = new xml2js.Parser();
                                 parser.parseString(row_data._xml, function(err,result){
                                     if(err){console.log(err)}
