c6943517c3cf90b5b9fdd6f518ad76386c4d3bcf
Adrian Green <adriangreen@Adrians-MacBook-Air.local>
2014-07-01 14:48:37 -0400

    counter issues

--- audit/admin.js
+++ audit/admin.js
@@ -133,8 +133,7 @@ app.post('/details', function(req,res){
                     
                     var detailsParse = JSON.parse(body);
                     if(detailsParse.mimeType == "application/vnd.google-apps.spreadsheet"){
-                        //var my_sheet = new GoogleSpreadsheet(detailsParse.id);
-                        var my_sheet = new GoogleSpreadsheet('1sA58w2JzUOj88Y5p14A5Rew5vyLyF5Xc4LbalYQQkuU');
+                        var my_sheet = new GoogleSpreadsheet(detailsParse.id);
                         my_sheet.setAuth('uploads@indmusicnetwork.com','ForThePeople', function(err){
                             my_sheet.getRows( 1, function(err, row_data){
                                 res.send(row_data);
