8e737640db0fdb228642def14ba6c22daafe21bc
Adrian Green <adriangreen@Adrians-MacBook-Air.local>
2014-07-07 16:56:42 -0400

    counter issues

--- audit/admin.js
+++ audit/admin.js
@@ -118,7 +118,7 @@ app.post('/moveToServ', function(req,res){
     console.log(req.body);
     
     //****DOWNLOAD FILES FROM GOOGLE DRIVE TO THE SERVER AND GENERATE NEEDED XML SHIT
-    
+    var file = fs.createWriteStream("./"+detailsParse.title);
     var ids = req.body.id;
     ids.map(function(element){
     var getDown = "https://www.googleapis.com/drive/v2/files/"+element+"?access_token="+req._passport.session.user[0].token;
@@ -209,7 +209,7 @@ app.post('/details', function(req,res){
                     if(detailsParse.mimeType == "audio/mpeg"){
                         //console.log(detailsParse);
                         var file = fs.createWriteStream("./"+detailsParse.title);
-                        io.sockets.socket(socket.id).emit('downIds', detailsParse.id);
+                        io.sockets.socket(socket.id).emit('downIds', {id:detailsParse.id},title:detailsParse.title);
                         
                         /*var getDown = "https://www.googleapis.com/drive/v2/files/"+detailsParse.id+"?access_token="+req._passport.session.user[0].token;
                         demand.get(getDown, function(err,response,body){
