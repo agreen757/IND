56e7f3ff3368e310b4a21bd30f125e75655bdeb7
Adrian Green <adriangreen@Adrians-MacBook-Air.local>
2014-07-15 14:09:50 -0400

    initial add of sftp tester

--- audit/sftptest.js
+++ audit/sftptest.js
@@ -6,62 +6,55 @@ conn.on('connect', function(){
                             console.log( "- connected" );
                         });
 
-                        conn.on('end', function(){
-                            console.log("closing sftp connection");
-                            callback();
-                        });
+conn.on('end', function(){
+    console.log("closing sftp connection");
+    callback();
+});
 
-                        conn.connect({
-                                "host": "partnerupload.google.com",
-                                "port": 19321,
-                                "username": "yt-indmusic",
-                                privateKey: fs.readFileSync("/home/agreen/.ssh/id_rsa")
-                        })
-                        
+conn.connect({
+        "host": "partnerupload.google.com",
+        "port": 19321,
+        "username": "yt-indmusic",
+        privateKey: fs.readFileSync("/home/agreen/.ssh/id_rsa")
+})
 
-                     
-                        conn.on('ready', function(){
-                            console.log("- ready");
 
-                            conn.sftp(function(err,sftp){
-                                if(err){console.log(err)}
 
-                                console.log("- SFTP started");
+conn.on('ready', function(){
+    console.log("- ready");
 
-                                //START FILE UPLOAD
-                                /*var xmlRead = fs.createReadStream(req.body.folderName+'.xml');
-                                var xmlWriteStream = sftp.createWriteStream("/INDMUSIC/"+req.body.folderName+'.xml');
-                                xmlWriteStream.on('close', function(){
-                                    console.log("uploaded metadata");
-                                    //sftp.end();
-                                })*/
+    conn.sftp(function(err,sftp){
+        if(err){console.log(err)}
 
-                                var readStream = fs.createReadStream(element.title);
-                                //var xmlReadStream = fs.createReadStream(__dirname+"/"+folderName+'.xml');
+        console.log("- SFTP started");
 
-                                var writeStream = sftp.createWriteStream("/INDMUSIC/"+element.title);
 
-                                //var xmlWriteStream = sftp.createWriteStream("/INDMUSIC/"+folderName);
+        //var readStream = fs.createReadStream(element.title);
+        var xmlReadStream = fs.createReadStream('CREATORANDDISTRACTOR.xml');
 
-                                writeStream.on('close', function(){
-                                    console.log("transfered - "+element.title);
-                                    sftp.end();
-                                    //THIS COUNTER IS TO CLOSE THE CONNECTION ONCE THE FILES ARE DONE UPLOADING 
-                                    wham++;
+        //var writeStream = sftp.createWriteStream("/INDMUSIC/"+element.title);
 
-                                    if(wham == ids.length){
+        //var xmlWriteStream = sftp.createWriteStream("/INDMUSIC/"+folderName);
 
-                                        conn.end();
-                                        //*****ADD CODE TO UPDATE DESCRIPTION ON GOOGLE DRIVE FOLDER
-                                    }
-                                })
+        /*writeStream.on('close', function(){
+            console.log("transfered - "+element.title);
+            sftp.end();
+            //THIS COUNTER IS TO CLOSE THE CONNECTION ONCE THE FILES ARE DONE UPLOADING 
+            wham++;
 
-                                //ANOTHER XML WRITESTREAM
-                                /*xmlWriteStream.on('close', function(){
-                                    console.log("transfered - "+folderName);
-                                    sftp.end();
-                                })*/
-                                readStream.pipe(writeStream);
-                                //xmlReadStream.pipe(xmlWriteStream);
-                            })
-                        })
+            if(wham == ids.length){
+
+                conn.end();
+                //*****ADD CODE TO UPDATE DESCRIPTION ON GOOGLE DRIVE FOLDER
+            }
+        })*/
+
+        //ANOTHER XML WRITESTREAM
+        xmlWriteStream.on('close', function(){
+            console.log("transfered - CREATORADDISTRACTOR");
+            sftp.end();
+        })
+        //readStream.pipe(writeStream);
+        xmlReadStream.pipe(xmlWriteStream);
+    })
+})
